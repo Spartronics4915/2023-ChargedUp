eabd802a7fe29a690ea34dbddad1195e05e138f9
ss0g <55930267+ss0g@users.noreply.github.com>
2023-01-08 20:51:30 -0800

    renamed frc directory to frc2023

--- src/main/java/com/spartronics4915/frc2023/robot/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2023/robot/RobotContainer.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package com.spartronics4915.frc.robot;
+package com.spartronics4915.frc2023.robot;
 
-import com.spartronics4915.frc.robot.Constants.OperatorConstants;
-import com.spartronics4915.frc.robot.commands.Autos;
-import com.spartronics4915.frc.robot.commands.ExampleCommand;
-import com.spartronics4915.frc.robot.subsystems.ExampleSubsystem;
+import com.spartronics4915.frc2023.robot.Constants.OperatorConstants;
+import com.spartronics4915.frc2023.robot.commands.Autos;
+import com.spartronics4915.frc2023.robot.commands.ExampleCommand;
+import com.spartronics4915.frc2023.robot.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
