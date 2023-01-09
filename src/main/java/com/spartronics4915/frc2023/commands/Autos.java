b63b77b3e171625be5bfa587f0ddf36190805661
ss0g <55930267+ss0g@users.noreply.github.com>
2023-01-08 20:58:44 -0800

    moved everything from frc2023/robot to frc2023

--- src/main/java/com/spartronics4915/frc2023/commands/Autos.java
+++ src/main/java/com/spartronics4915/frc2023/commands/Autos.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package com.spartronics4915.frc2023.robot.commands;
+package com.spartronics4915.frc2023.commands;
 
-import com.spartronics4915.frc2023.robot.subsystems.ExampleSubsystem;
+import com.spartronics4915.frc2023.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
