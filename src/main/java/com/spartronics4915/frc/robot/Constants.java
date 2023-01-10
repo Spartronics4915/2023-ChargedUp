71c8136d6fd9c8cc266f3d8af096fe3ffbaca257
Zeckten <64096716+Zeckten@users.noreply.github.com>
2023-01-09 18:26:54 -0800

    Created PhotonCameraWrapper and PrintPos

--- src/main/java/com/spartronics4915/frc/robot/Constants.java
+++ src/main/java/com/spartronics4915/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package com.spartronics4915.frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
@@ -16,8 +21,26 @@ package com.spartronics4915.frc.robot;
  * wherever the
  * constants are needed, to reduce verbosity.
  */
+
+ 
 public final class Constants {
     public static class OperatorConstants {
         public static final int kDriverControllerPort = 0;
+    }
+
+    static class FieldConstants {
+        static final double length = Units.feetToMeters(54);
+        static final double width = Units.feetToMeters(27);
+    }
+
+    static class VisionConstants {
+        static final Transform3d robotToCam =
+                new Transform3d(
+                        new Translation3d(0.5, 0.0, 0.5),
+                        new Rotation3d(
+                                0, 0,
+                                0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
+        // from center.
+        static final String cameraName = "YOUR CAMERA NAME";
     }
 }
---
+++ src/main/java/com/spartronics4915/frc/robot/commands/PrintPos.java
@@ -0,0 +1,51 @@
+package com.spartronics4915.frc.robot.commands;
+
+import com.spartronics4915.frc.robot.subsystems.ExampleSubsystem;
+import com.spartronics4915.frc.robot.PhotonCameraWrapper;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class PrintPos extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private final ExampleSubsystem m_subsystem;
+    private PhotonCameraWrapper cam;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public PrintPos(ExampleSubsystem subsystem) {
+        m_subsystem = subsystem;
+        cam = new PhotonCameraWrapper();
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        Pose2d result = cam.getEstimatedGlobalPose(new Pose2d()).getFirst();
+        Double[] split = {result.getX(), result.getY()};
+        SmartDashboard.putNumberArray("Pose2D", split);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
