dd4693086dfecb613272ec2f82210bfb79098a93
otter502 <115671877+otter502@users.noreply.github.com>
2023-03-07 18:23:23 -0800

    minor formatting changes

--- src/main/java/com/spartronics4915/frc2023/subsystems/MotorAbsEncoderComboSubsystem.java
+++ src/main/java/com/spartronics4915/frc2023/subsystems/MotorAbsEncoderComboSubsystem.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * Understanding native and arm coordinate systems:
  * To reason about motions, it is most conventient to put 0 degrees at horizontal and use CCW coordinates
  * So +30 is up and -30 is down.  But, this puts wrap-around right at horizontal.
- * These are called "arm coordinates".
+ * These are called "horizon coordinates".
  * To avoid having to handle wraparound everywhere, the encoder is actually set so that horizontal is at 180.
  * This gives 180 degrees up and down and avoids having to think about handling warp-around
  * These are "native coordinates", the coordinates of the actual encoder
@@ -192,43 +192,43 @@ public class MotorAbsEncoderComboSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
+        double angleWithEarth = ((mAngleProvider == null) ?  getHorizonPosition() : mAngleProvider.getAngleWithEarth()).getRadians();
 
-            double angleWithEarth = ((mAngleProvider == null) ?  getHorizonPosition() : mAngleProvider.getAngleWithEarth()).getRadians();
+        // double currVelocity = getVelocity().getRadians();
+        double currPosNative = getNativePosition().getRadians();
 
-            // double currVelocity = getVelocity().getRadians();
-            double currPosNative = getNativePosition().getRadians();
+        // var currState = new TrapezoidProfile.State(mModeledPosition, mModeledVelocity);
+        // var goalState = new TrapezoidProfile.State(mReferenceRadians, 0);
+        // TrapezoidProfile currMotionProfile = new TrapezoidProfile(motionConstraints, goalState, currState);
+        // double ticLength = 1./50; // Robot runs at 50Hz
+        // var state = currMotionProfile.calculate(ticLength);
 
-            // var currState = new TrapezoidProfile.State(mModeledPosition, mModeledVelocity);
-            // var goalState = new TrapezoidProfile.State(mReferenceRadians, 0);
-            // TrapezoidProfile currMotionProfile = new TrapezoidProfile(motionConstraints, goalState, currState);
-            // double ticLength = 1./50; // Robot runs at 50Hz
-            // var state = currMotionProfile.calculate(ticLength);
+        // // Trapezoid profiling not working, so this effectively disables it.
+        
+        double pidReferenceRadians = mReferenceRadians;//state.position;
+        //trapezoidTarget = state.position;
 
-            // // Trapezoid profiling not working, so this effectively disables it.
-            
-            double pidReferenceRadians = mReferenceRadians;//state.position;
-            //trapezoidTarget = state.position;
+        //mModeledVelocity = state.velocity;
+        // By doing this, we are just playing back the motion profile in case the PID controller doesn't keep up.
+        //mModeledPosition = state.position;
+        // currPosArm=nativeToArm(Rotation2d.fromDegrees(180)).getRadians();
+        // currPosNative = Rotation2d.fromDegrees(180).getRadians();
 
-            //mModeledVelocity = state.velocity;
-            // By doing this, we are just playing back the motion profile in case the PID controller doesn't keep up.
-            //mModeledPosition = state.position;
-            // currPosArm=nativeToArm(Rotation2d.fromDegrees(180)).getRadians();
-            // currPosNative = Rotation2d.fromDegrees(180).getRadians();
+        double ffComponent = -kFF * Math.cos(angleWithEarth);
+        double err = kP*(pidReferenceRadians - currPosNative);
 
-            double ffComponent = -kFF * Math.cos(angleWithEarth);
-            double err = kP*(pidReferenceRadians - currPosNative);
+        double total_output = ffComponent + err;
 
-            double total_output = ffComponent + err;
-
-            if(total_output > 1.0) {
-                total_output = 1;
-            } else if (total_output < -1.0) {
-                total_output = -1.0;
-            }
-            
-            if(mActive && mReferenceSet) {
-                mMotor.set(total_output);
-                mLastSpeedOutput = total_output;
+        if(total_output > 1.0) {
+            total_output = 1;
+        } else if (total_output < -1.0) {
+            total_output = -1.0;
+        }
+        
+        
+        if(mActive && mReferenceSet) {
+            mMotor.set(total_output);
+            mLastSpeedOutput = total_output;
         }
     }
 
