44563df33e6a972d71e48c08986179cf6062c27d
otter502 <115671877+otter502@users.noreply.github.com>
2023-03-08 13:57:01 -0800

    made a abstract class that wraps subsystem base for arm joints made PivotSubsystem and WristSubsystem subclasses of them

---
+++ src/main/java/com/spartronics4915/frc2023/subsystems/ArmJointAbstractSubsystem.java
@@ -0,0 +1,143 @@
+package com.spartronics4915.frc2023.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.spartronics4915.frc2023.Constants.Arm.ArmMotorConstants;
+import com.spartronics4915.frc2023.Constants.Arm.CanSparkMaxMotorConstants;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class ArmJointAbstractSubsystem extends SubsystemBase{
+
+    protected CANSparkMax mMotor;
+    protected SparkMaxAbsoluteEncoder mAbsEncoder;
+    protected SparkMaxPIDController mPIDController;
+    protected final ArmMotorConstants kConstants;
+    protected Rotation2d currentRefrence;
+    protected boolean setRef;
+
+    public ArmJointAbstractSubsystem(ArmMotorConstants constants) {
+        super();
+        kConstants = constants;
+        motorInit();
+        encoderInit();
+        pidControllerInit();
+        setRef = false;
+        currentRefrence = HorizonToNative(Rotation2d.fromDegrees(0));
+        
+    }
+
+    protected void motorInit() {
+        CanSparkMaxMotorConstants motorConstants =  kConstants.kMotorConstants;
+        mMotor = new CANSparkMax(motorConstants.kMotorID, motorConstants.kMotorType);
+        mMotor.setIdleMode(motorConstants.kIdleMode);
+        mMotor.setInverted(motorConstants.kInverted);
+
+        mMotor.setSmartCurrentLimit(20);
+    }
+    
+    protected void encoderInit() {
+        CanSparkMaxMotorConstants motorConstants =  kConstants.kMotorConstants;
+        mAbsEncoder = mMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
+        mAbsEncoder.setZeroOffset(motorConstants.kZeroOffset.getRotations()); //this is in rotation because the encoder's position conversion factor hasn't been set yet
+        mAbsEncoder.setPositionConversionFactor(motorConstants.kPositionConversionFactor);
+        mAbsEncoder.setInverted(motorConstants.kInverted);
+    }
+
+    protected void pidControllerInit() {
+        mPIDController = mMotor.getPIDController(); 
+        
+        mPIDController.setFeedbackDevice(mAbsEncoder);
+        
+        mPIDController.setP(kConstants.kPIDConstants.kP);
+        mPIDController.setI(kConstants.kPIDConstants.kI);
+        mPIDController.setD(kConstants.kPIDConstants.kD);
+        mPIDController.setFF(kConstants.kPIDConstants.kFF);
+
+        mPIDController.setSmartMotionMaxAccel(kConstants.kSmartMotionsConstants.kSmartMotionMaxAccel, 0);
+        mPIDController.setSmartMotionMaxVelocity(kConstants.kSmartMotionsConstants.kSmartMotionMaxVelocity, 0);
+        mPIDController.setSmartMotionMinOutputVelocity(kConstants.kSmartMotionsConstants.kSmartMotionMinOutputVelocity, 0);
+    }
+
+
+    //unit conversions:
+    public Rotation2d HorizonToNative(Rotation2d armRotation) {
+        var outputRot = armRotation.unaryMinus().plus(kConstants.kHorizonDeflection);
+
+        // To match the encoder, we want angles in [0, 2PI]
+        var radValue = outputRot.getRadians();
+        if (radValue < 0) {
+            outputRot = Rotation2d.fromRadians(radValue + 2 * Math.PI);
+        }
+
+        return outputRot;
+    }
+
+    public Rotation2d nativeToHorizon(Rotation2d nativeRotation) {
+        return nativeRotation.minus(kConstants.kHorizonDeflection).unaryMinus();
+    }
+
+    // getters for position:
+
+    public Rotation2d getHorizonPosition() {
+        return nativeToHorizon(getNativePosition());
+    }
+
+    public Rotation2d getNativePosition() {
+        return Rotation2d.fromRadians(mAbsEncoder.getPosition());
+    }
+
+    //setters for position:
+    public void setHorizonReference(Rotation2d ref) {
+        //TODO add saftey method
+        setNativeReference(HorizonToNative(ref));
+    }
+
+    private void setNativeReference(Rotation2d ref) {
+        if(isNativeRefSafe(ref)){
+            currentRefrence = ref;
+            setRef = true;
+        } else {
+            onUnsafeNativeRef(ref);
+        }
+    }
+
+    abstract protected boolean isNativeRefSafe(Rotation2d ref);
+
+    abstract protected void onUnsafeNativeRef(Rotation2d ref);
+
+    //periodic methodds:
+    abstract protected double calculateFeedforwardValue();
+
+    @Override
+    public void periodic() {
+        super.periodic();
+        if (setRef)
+            mPIDController.setReference(
+                currentRefrence.getRadians(), 
+                ControlType.kSmartMotion, 0, 
+                calculateFeedforwardValue()
+            );
+    }
+
+    //getters for debug teleop:
+    public CANSparkMax getMotor() {
+        return mMotor;
+    }
+
+    public SparkMaxAbsoluteEncoder getAbsEncoder() {
+        return mAbsEncoder;
+    }
+
+     /**
+     * @return the refrence the PID controller is in horizon angles
+     */
+    public Rotation2d getCurrentRefrence() {
+        return currentRefrence;
+    }
+
+}
+
--- src/main/java/com/spartronics4915/frc2023/subsystems/PivotSubsystem.java
+++ src/main/java/com/spartronics4915/frc2023/subsystems/PivotSubsystem.java
@@ -1,140 +1,28 @@
 package com.spartronics4915.frc2023.subsystems;
 
-import com.spartronics4915.frc2023.Constants.Arm.ArmMotorConstants;
-import com.spartronics4915.frc2023.Constants.Arm.CanSparkMaxMotorConstants;
 import com.spartronics4915.frc2023.Constants.Arm;
-
-import com.revrobotics.CANSparkMax;
-
-import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.spartronics4915.frc2023.subsystems.ArmJointAbstractSubsystem;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PivotSubsystem extends SubsystemBase {
-    private CANSparkMax mMotor;
-    private SparkMaxAbsoluteEncoder mAbsEncoder;
-    private SparkMaxPIDController mPIDController;
-    private final ArmMotorConstants kConstants;
-    private Rotation2d currentRefrence;
-    private boolean setRef;
+public class PivotSubsystem extends ArmJointAbstractSubsystem {
     
-    //important notes: the mpidcontroller can only work in native units
-
     public PivotSubsystem() {
-        super();
-        kConstants = Arm.kPivotMotorConstants;
-        motorInit();
-        encoderInit();
-        pidControllerInit();
-        setRef = false;
-        currentRefrence = new Rotation2d(0);
+        super(Arm.kPivotMotorConstants);
     }
 
-    //individual init methods:
-
-    private void motorInit() {
-        CanSparkMaxMotorConstants motorConstants =  kConstants.kMotorConstants;
-        mMotor = new CANSparkMax(motorConstants.kMotorID, motorConstants.kMotorType);
-        mMotor.setIdleMode(motorConstants.kIdleMode);
-        mMotor.setInverted(motorConstants.kInverted);
-
-        mMotor.setSmartCurrentLimit(20);
-    }
-    
-    private void encoderInit() {
-        CanSparkMaxMotorConstants motorConstants =  kConstants.kMotorConstants;
-        mAbsEncoder = mMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
-        mAbsEncoder.setZeroOffset(motorConstants.kZeroOffset.getRotations()); //this is in rotation because the encoder's position conversion factor hasn't been set yet
-        mAbsEncoder.setPositionConversionFactor(motorConstants.kPositionConversionFactor);
-        mAbsEncoder.setInverted(motorConstants.kInverted);
-    }
-
-    private void pidControllerInit() {
-        mPIDController = mMotor.getPIDController(); 
-        
-        mPIDController.setFeedbackDevice(mAbsEncoder);
-        
-        mPIDController.setP(kConstants.kPIDConstants.kP);
-        mPIDController.setI(kConstants.kPIDConstants.kI);
-        mPIDController.setD(kConstants.kPIDConstants.kD);
-        mPIDController.setFF(kConstants.kPIDConstants.kFF);
-
-        mPIDController.setSmartMotionMaxAccel(kConstants.kSmartMotionsConstants.kSmartMotionMaxAccel, 0);
-        mPIDController.setSmartMotionMaxVelocity(kConstants.kSmartMotionsConstants.kSmartMotionMaxVelocity, 0);
-        mPIDController.setSmartMotionMinOutputVelocity(kConstants.kSmartMotionsConstants.kSmartMotionMinOutputVelocity, 0);
-    }
-
-    //unit conversions:
-
-    public Rotation2d HorizonToNative(Rotation2d armRotation) {
-        var outputRot = armRotation.unaryMinus().plus(kConstants.kHorizonDeflection);
-
-        // To match the encoder, we want angles in [0, 2PI]
-        var radValue = outputRot.getRadians();
-        if (radValue < 0) {
-            outputRot = Rotation2d.fromRadians(radValue + 2 * Math.PI);
-        }
-
-        return outputRot;
-    }
-
-    public Rotation2d nativeToHorizon(Rotation2d nativeRotation) {
-        return nativeRotation.minus(kConstants.kHorizonDeflection).unaryMinus();
-    }
-    
-    //getters for position
-    public Rotation2d getHorizonPosition() {
-        return nativeToHorizon(getNativePosition());
-    }
-
-    public Rotation2d getNativePosition() {
-        return Rotation2d.fromRadians(mAbsEncoder.getPosition());
-    }
-
-    //setters for position:
-    public void setHorizonReference(Rotation2d ref) {
-        // This is designed to ignore unsafe arm positions.
-        setNativeReference(HorizonToNative(ref));
-    }
-
-    private void setNativeReference(Rotation2d ref) {
-        currentRefrence = ref;
-        setRef = true;
-        // mPIDController.setReference(ref.getRadians(), ControlType.kSmartMotion, 0, Math.cos(getHorizonPosition().getRadians()));
-    }
-
-    //feedforward update
     @Override
-    public void periodic() {
-        super.periodic();
-        if (setRef)
-            mPIDController.setReference(
-                currentRefrence.getRadians(), 
-                ControlType.kSmartMotion, 0, 
-                Math.cos(getHorizonPosition().getRadians())
-            );
-
+    protected double calculateFeedforwardValue() {
+        return Math.cos(getHorizonPosition().getRadians());
     }
 
-    //getters for debug teleop:
-    public CANSparkMax getMotor() {
-        return mMotor;
+    @Override
+    protected boolean isNativeRefSafe(Rotation2d ref) {
+        return true;
     }
 
-    public SparkMaxAbsoluteEncoder getAbsEncoder() {
-        return mAbsEncoder;
+    @Override
+    protected void onUnsafeNativeRef(Rotation2d ref) {
+        System.out.println("unsafe ref, not setting");
     }
-
-    /**
-     * @return the refrence the PID controller is in horizon angles
-     */
-    public Rotation2d getCurrentRefrence() {
-        return currentRefrence;
-    }
-
-    
 }
