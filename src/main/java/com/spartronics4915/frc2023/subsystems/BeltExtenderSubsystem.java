194e99de34eb38a02736f3a45fe1469b57ee3ee1
ss0g <55930267+ss0g@users.noreply.github.com>
2023-03-27 17:38:14 -0700

    change belt conversion constant

--- src/main/java/com/spartronics4915/frc2023/subsystems/BeltExtenderSubsystem.java
+++ src/main/java/com/spartronics4915/frc2023/subsystems/BeltExtenderSubsystem.java
@@ -24,7 +24,7 @@ public class BeltExtenderSubsystem extends SubsystemBase  {
     private final int kMotorID;
     private CANSparkMax mMotor;
     public RelativeEncoder mEncoder;
-    private final double kRevPerInch = 13.25/14.19;
+    private final double kRevPerInch = 13.25/14.19 * (9. / 15.);
     public SparkMaxPIDController mPIDController;
 
     private DigitalInput mLimitSwitchZero;
